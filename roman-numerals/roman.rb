e0d157d6503ee45915fdfe1b2a1d772d18b0078d
awortham <awortham@newlifechurch.tv>
2015-01-04 17:31:14 -0700

    adding hash

--- roman-numerals/roman.rb
+++ roman-numerals/roman.rb
@@ -1,14 +1,15 @@
 class Fixnum
   def to_roman
-    if self == 1
-      'I'
-    elsif self == 2
-      'II'
-    elsif self == 3
-      'III'
-    else
-      'IV'
-    end
+    romans = { 1 => 'I',
+               5 => 'V',
+               10 => 'X',
+               50 =>'L',
+               100 => 'C',
+               500 => 'D',
+               1000 => 'M'
+               }
+    romans[self]
 
   end
+
 end
