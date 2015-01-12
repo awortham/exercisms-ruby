19b9a1c115ec5a56d59d91ac9f67448b2691cdca
awortham <wortham.aaron@gmail.com>
2015-01-11 21:14:05 -0700

    not quite right

--- nth-prime/prime.rb
+++ nth-prime/prime.rb
@@ -1,4 +1,16 @@
+#very poor implementation. not sure if it works as it takes to long to run code
+require 'prime'
 class Prime
-  def self.nth(num)
+  def self.nth(spot)
+    answer = {}
+    Prime.each_with_index do |num, index|
+      answer[num] = index
+    end
+    final = ''
+    answer.keys.each do |key|
+      if key == spot
+        final << answer[key]
+      end
+    end
   end
 end
