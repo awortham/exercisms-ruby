7f2b185ef4fd131def66ab73259c3633fab29abf
awortham <wortham.aaron@gmail.com>
2015-01-12 18:26:59 -0700

    much better

--- nth-prime/prime.rb
+++ nth-prime/prime.rb
@@ -1,16 +1,10 @@
-#very poor implementation. not sure if it works as it takes to long to run code
 require 'prime'
+
 class Prime
   def self.nth(spot)
-    answer = {}
     Prime.each_with_index do |num, index|
-      answer[num] = index
-    end
-    final = ''
-    answer.keys.each do |key|
-      if key == spot
-        final << answer[key]
-      end
+      raise ArgumentError if spot == 0
+       return num if index == spot - 1
     end
   end
 end
