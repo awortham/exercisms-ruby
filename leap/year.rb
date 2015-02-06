6aac07cfc3b85d5f93ab2b8997b8c5b4518a426f
awortham <wortham.aaron@gmail.com>
2015-02-05 21:01:19 -0700

    refactored

--- leap/year.rb
+++ leap/year.rb
@@ -1,12 +1,6 @@
 class Year
   def self.leap?(year)
-    if by_4?(year) && !by_100?(year)
-      true
-    elsif by_100?(year) && by_400?(year)
-      true
-    else
-      false
-    end
+    by_4?(year) && (!by_100?(year) || by_400?(year))
   end
 
   def self.by_4?(year)
@@ -19,13 +13,5 @@ class Year
 
   def self.by_400?(year)
     year % 400 == 0
-  end
-
-  def self.negative(year)
-    "No, #{year} is not a leap year"
-  end
-
-  def self.positive(year)
-    "Yes, #{year} is a leap year"
   end
 end
